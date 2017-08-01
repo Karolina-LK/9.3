3301f672cace444a8a1c763f4a9b3dfb8af0bf3f
Karolina Lubieniecka <lubieniecka@interia.pl>
2017-08-01 17:07:10 +0200

    added  files of dinosaurs, changing the word

---
+++ 93.js
@@ -0,0 +1,8 @@
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+
+var dinosaur = 'triceratops';
+var dinosaurUpperCased = dinosaur.toUpperCase();
+var textReplaced = text.replace('Velociraptor', dinosaurUpperCased);
+
+console.log(text.length/2);
+console.log(text.slice(0,73));
